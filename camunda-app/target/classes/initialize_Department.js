819a1245cb1860ead80bef9d97d723d22647115f
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-08-06 00:43:08 +0530

    set varibale by using Exceution object

--- camunda-app/target/classes/initialize_Department.js
+++ camunda-app/target/classes/initialize_Department.js
@@ -2,6 +2,11 @@ print("The User Object inisde the initilize department.js:: "+ JSON.stringify(us
 print("The User Object 2 inisde the initilize department.js:: "+ JSON.stringify(userObj2));
 print("The User Object 3 inisde the initilize department.js:: "+ JSON.stringify(userObj3));
 
+
+print("The User Object 4 inisde the initilize department.js:: "+ JSON.stringify(userObj4));
+
+print("The User Object 4 inisde the initilize department.js:: suing execution variable   "+ JSON.stringify(execution.getVariable('userObj4')));
+
 var dept={
 	id: "29",
 	department :"Computer"
