3c0fab17ebd2b19798736ee6d11acaa9ca089bb6
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-08-06 00:29:51 +0530

    returning single and multiple object from scripted task

--- camunda-app/src/main/resources/initialize_Department.js
+++ camunda-app/src/main/resources/initialize_Department.js
@@ -1,3 +1,7 @@
+print("The User Object inisde the initilize department.js:: "+ JSON.stringify(userObj));
+print("The User Object 2 inisde the initilize department.js:: "+ JSON.stringify(userObj2));
+print("The User Object 3 inisde the initilize department.js:: "+ JSON.stringify(userObj3));
+
 var dept={
 	id: "29",
 	department :"Computer"
